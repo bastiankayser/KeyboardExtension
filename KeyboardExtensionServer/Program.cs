2a9dc22c694af0d3c5e7b55125b1013a1395f963
Bastian Kayser <bastian.kayser@motognosis.com>
2015-12-11 19:51:56 +0100

    Initial Commit
    Initial commit

---
+++ KeyboardExtensionServer/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace KeyboardExtensionServer
+{
+    class Program
+    {
+        private WindowListener windowListener;
+        private SimpleHTTPServer simpleHttpServer;
+        string myFolder = @"D:\temp\testwebsite";
+
+        public Program()
+        {
+            windowListener = new WindowListener();
+
+            //simpleHttpServer = new SimpleHTTPServer(myFolder,8008);
+        }
+
+        static void Main(string[] args)
+        {
+
+            //using (Microsoft.Owin.Hosting.WebApp.Start<Startup>("http://localhost:9000"))
+            //{
+            //    Console.WriteLine("Press [enter] to quit...");
+            //    Console.ReadLine();
+            //}
+
+            Program prog = new Program();
+
+            Application.Run(); //<----
+            Console.ReadKey();
+            //prog.simpleHttpServer.Stop();
+        }
+
+
+    }
+}
+
+
+//    class Program : ServiceBase
+//    {
+
+//        static void Main(string[] args)
+//        {
+//            Thread.Sleep(5000); // Test it with 5 Seconds, set a window to foreground, and you see it works!
+//            IntPtr hWnd = GetForegroundWindow();
+//            uint procId = 0;
+//            GetWindowThreadProcessId(hWnd, out procId);
+//            var proc = Process.GetProcessById((int)procId);
+//            Console.WriteLine(proc.MainModule);
+//            SendKeys.SendWait("^S");
+//            Console.ReadKey();
+//        }
+//        [DllImport("user32.dll")]
+//        public static extern IntPtr GetForegroundWindow();
+
+//        [DllImport("user32.dll", SetLastError = true)]
+//        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+//    }
+
+//}
---
+++ KeyboardExtensionServer/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Owin;
+
+namespace KeyboardExtensionServer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/html";
+                var strReader = new StreamReader(new FileStream("index.html", FileMode.Open));
+                var readToEnd = strReader.ReadToEnd();
+                strReader.Close();
+                return context.Response.WriteAsync(readToEnd);
+            });
+        }
+    }
+}
